c168e1fdc5600c4ac00fae633c8f1441b25d8738
raunov <rauno@rubio.ee>
2023-11-27 16:42:40 +0000

    fixed repo_path issue

--- check-change.py
+++ check-change.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from markdownify import markdownify as md
 from datetime import datetime
 import git
+import os
 
 def fetch_and_convert(url):
     response = requests.get(url)
@@ -12,25 +13,25 @@ def fetch_and_convert(url):
     return md(str(content))
 
 def has_content_changed(new_content, repo_path, current_version_file):
-    with open(f"{repo_path}/{current_version_file}", 'r',encoding='utf-8') as file:
+    with open(f"{repo_path}/versions/{current_version_file}", 'r',encoding='utf-8') as file:
         current_content = file.read()
     return current_content != new_content
 
 def main():
     url = 'https://www.casadebaile.ee/lepingutekst-klubiga-liitujatele/' 
-    repo_path = 'versions' # e.g. /home/user/my-repo
+    repo_path = os.getcwd() # e.g. /home/user/my-repo
     current_version_file = 'current.md'
     new_content = fetch_and_convert(url)
 
     if has_content_changed(new_content, repo_path, current_version_file):
         date_suffix = datetime.now().strftime("%Y%m%d")
-        with open(f"{repo_path}/version_{date_suffix}.md", 'w',encoding='utf-8') as file:
+        with open(f"{repo_path}/versions/version_{date_suffix}.md", 'w',encoding='utf-8') as file:
             file.write(new_content)
-        with open(f"{repo_path}/{current_version_file}", 'w',encoding='utf-8') as file:
+        with open(f"{repo_path}/versions/{current_version_file}", 'w',encoding='utf-8') as file:
             file.write(new_content)
         repo = git.Repo(repo_path)
         repo.git.add(A=True)
-        repo.git.commit('-m', 'Updated content version')
+        repo.git.commit('-m', 'Lepingumuudatused')
         repo.git.push()
 
 if __name__ == "__main__":
